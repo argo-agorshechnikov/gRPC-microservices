19f2846bfa7203f3dee5d0a026af3728b650c4b5
agorshechnikov <arkadiy.gorshechnikov@yandex.com>
2025-06-03 23:20:31 +0500

    add docker for product service

---
+++ cmd/product_service/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	prodpb "github.com/argo-agorshechnikov/gRPC-microservices/api/product-service"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	prodpb.UnimplementedProductServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed listen: %v", err)
+	}
+	s := grpc.NewServer()
+	prodpb.RegisterProductServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed serve: %v", err)
+	}
+}
---
+++ docker/product-service.Dockerfile
@@ -0,0 +1,21 @@
+FROM golang:1.24.2 AS builder
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY ./cmd/product_service ./cmd/product_service
+COPY ./internal ./internal
+COPY ./api ./api
+COPY ./pkg ./pkg
+
+RUN CGO_ENABLED=0 GOOS=linux go build -o product_service ./cmd/product_service/main.go
+
+FROM alpine:latest
+
+COPY --from=builder /app/product_service /usr/local/bin/product_service
+
+EXPOSE 50051
+
+ENTRYPOINT [ "/usr/local/bin/product_service" ]
